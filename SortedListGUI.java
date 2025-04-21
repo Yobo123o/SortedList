a81bdb4a6e10e105ac1c1b152c3ea2d3a2bcf798
bswartz <bswartz@ohioschoolboards.org>
2025-04-21 10:46:51 -0400

    Project files

--- SortedListGUI.java
+++ SortedListGUI.java
@@ -1,7 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 /**
  * GUI for inserting and searching strings in a sorted list.
